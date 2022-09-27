7fc289b504437daf4ae60b1515a61096b6f02f1a
pantshaswat <111427682+pantshaswat@users.noreply.github.com>
2022-09-27 21:29:11 +0545

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,18 +1,26 @@
 #include "mainwindow.h"
+#include "passbackup.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include "registration.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/Shaswat/OneDrive/Pictures/fg.png");
+    ui->label_6->setPixmap(pix);
+    QPixmap pic("C:/Users/Shaswat/OneDrive/Pictures/Screenshots/routine.jpg");
+    ui->label_4->setPixmap(pic);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 
 
 void MainWindow::on_pushButton_clicked()
@@ -22,11 +30,32 @@ void MainWindow::on_pushButton_clicked()
 
     if( username =="Test" && password=="Test"){
         QMessageBox::information(this,"Login","Username and password is correct");
-
+        MainWindow::close();
     }
     else{
-        QMessageBox::information(this,"Login","Username or password is not correct");
+       QMessageBox::StandardButton reply = QMessageBox::critical(this,"Login","Username or password is not correct \n \n Try again?",QMessageBox::Yes | QMessageBox:: No);
+        if (reply == QMessageBox::Yes){
 
-    }
+        }
+        else QApplication:: quit();
+}
+}
+
+
+
+void MainWindow::on_commandLinkButton_2_clicked()
+{
+    registration registration;
+    registration.setModal (true);
+    registration.exec();
+
+}
+
+
+void MainWindow::on_commandLinkButton_clicked()
+{
+    passbackup passbackup;
+    passbackup.setModal(true);
+    passbackup.exec();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,10 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_commandLinkButton_2_clicked();
+
+    void on_commandLinkButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
